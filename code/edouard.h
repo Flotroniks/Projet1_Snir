410304b0484e48eec625ef6f5704ce1625e7b7ae
Flotroniks <47397500+Flotroniks@users.noreply.github.com>
2021-03-24 07:43:20 +0100

    Add files via upload

--- code/edouard.h
+++ code/edouard.h
@@ -4,7 +4,10 @@
 
 
 int testf();
-
+int line ();
 int alea_dice(); //nombre aleatoire entre 1 et 6
-int nb_player(); // nombre de joueur
-int logo();
+int ask_nb_player(); // demande le nombre de joueur
+int logo();//logo
+int line_break(int);
+int rules();
+int scoreboard(int, int);
--- code/main.c
+++ code/main.c
@@ -5,18 +5,23 @@
 #include "edouard.h"
 #include "phillipe.h"
 
+int nb_player=0;
+int score[6];
+
+
 int main()
 {
 	srand( time( NULL ) );
 	/* code */
-	logo();
+	line();
+	//logo();
+	//line();
+	//rules();
 	
+	nb_player=ask_nb_player();
+	line_break(6);	
+	scoreboard(nb_player, *score);
 
-
-	
-	
-		
-	
 
 	return 0;
 }
